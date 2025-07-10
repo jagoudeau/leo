8ad6f09163cc7fdb56374066498dacda2ba52379
jagoudeau <85856420+jagoudeau@users.noreply.github.com>
2025-07-09 21:22:47 -0500

    Create models.js

---
+++ models.js
@@ -0,0 +1,14 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({ dialect: 'sqlite', storage: 'chatlogs.sqlite' });
+
+const ChatLog = sequelize.define('ChatLog', {
+  user: DataTypes.STRING,
+  message: DataTypes.TEXT,
+  reply: DataTypes.TEXT,
+  timestamp: DataTypes.DATE
+});
+
+async function initDb() {
+  await sequelize.sync();
+}
+module.exports = { ChatLog, initDb, sequelize };
