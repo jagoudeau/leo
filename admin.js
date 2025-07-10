f5320c47e5c72774a9eea706e55614ed37c50687
jagoudeau <85856420+jagoudeau@users.noreply.github.com>
2025-07-09 21:23:10 -0500

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,28 @@
+const AdminJS = require('adminjs');
+const AdminJSExpress = require('@adminjs/express');
+const { sequelize, ChatLog } = require('./models');
+const expressBasicAuth = require('express-basic-auth');
+
+AdminJS.registerAdapter(require('@adminjs/sequelize'));
+
+async function setupAdmin(app) {
+  const admin = new AdminJS({
+    databases: [sequelize],
+    rootPath: '/admin'
+  });
+
+  const router = AdminJSExpress.buildAuthenticatedRouter(admin, {
+    authenticate: async (email, password) =>
+      email === process.env.ADMIN_USER && password === process.env.ADMIN_PASS
+        ? { email }
+        : null,
+    cookiePassword: process.env.COOKIE_PASS || 'supersecret'
+  }, null, {
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.COOKIE_PASS || 'supersecret'
+  });
+
+  app.use(admin.options.rootPath, router);
+}
+module.exports = setupAdmin;
